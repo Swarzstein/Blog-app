8b4cad7649f530e3611cb8e30a1e03a1e505626a
Swarzstein <zorrillagja@gmail.com>
2023-06-17 22:22:23 -0400

    tests passing

--- db/migrate/20230617143953_create_comments.rb
+++ db/migrate/20230617143953_create_comments.rb
@@ -2,8 +2,8 @@ class CreateComments < ActiveRecord::Migration[7.0]
   def change
     create_table :comments do |t|
       t.string :text
-      t.references :user, foreign_key: true
       t.references :author, foreign_key: { to_table: :users }
+      t.references :post, foreign_key: true
 
       t.timestamps
     end
