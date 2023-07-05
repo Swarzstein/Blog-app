f5f4c9d14bf1dedf8281cd3d58410505cecb93e9
getssh <gtamrat33@gmail.com>
2023-07-06 00:29:13 +0300

    Update user data for inegration testing

--- spec/integration/users_index_spec.rb
+++ spec/integration/users_index_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'Users Index', type: :feature do
   before :each do
-    author1 = User.create(name: 'Malik44', photo: 'https://malik44.jpg', bio: 'Sailor')
+    author1 = User.create(name: 'Abebe', photo: 'https://Abebe.jpg', bio: 'Abebe bio')
     author2 = User.create(name: 'Akai321', photo: 'https://Akaiiii.jpg', bio: 'Martial Artist')
     Post.create(title: 'Legacy', text: 'The legacy of the righteous is a blessing', author_id: author1.id)
     Post.create(title: 'The way of the Water',
@@ -15,13 +15,13 @@ RSpec.describe 'Users Index', type: :feature do
 
   scenario 'I can see the username of all other users' do
     visit '/users'
-    expect(page).to have_content('Malik44')
+    expect(page).to have_content('Abebe')
     expect(page).to have_content('Akai321')
   end
 
   scenario 'I can see the profile picture for each user' do
     visit '/users'
-    expect(page).to have_css("img[src*='https://malik44.jpg']")
+    expect(page).to have_css("img[src*='https://Abebe.jpg']")
     expect(page).to have_css("img[src*='https://Akaiiii.jpg']")
   end
 
@@ -33,9 +33,9 @@ RSpec.describe 'Users Index', type: :feature do
 
   scenario "When I click on a user, I am redirected to that user's show page" do
     visit '/users'
-    click_link 'Malik44'
-    expect(page).to have_content('Malik44')
-    expect(page).to have_content('Sailor')
+    click_link 'Abebe'
+    expect(page).to have_content('Abebe')
+    expect(page).to have_content('Abebe bio')
     expect(page).to have_content('Legacy')
   end
 end
