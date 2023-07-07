e06633b0d3ae69a53d43df731d859914f645c08e
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-07-07 19:05:52 +0300

    Add logout link to user index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     @current_user = current_user
-    # take the active session id
-    @session_id = session[:session_id]
   end
 
   def show
@@ -11,16 +9,3 @@ class UsersController < ApplicationController
     @posts = @user.most_recent_posts
   end
 end
-
-# def new
-#  @user = User.new
-# end
-
-# def create
-#  @user = User.create(params[:user])
-#  if @user.save
-#    redirect_to user_path(@user.id)
-#  else
-#    render :new
-#  end
-# end
